a7da54bcf122676cc25a1a30c3d355ca4e97e34a
Rauul2967 <63261160+Rauul2967@users.noreply.github.com>
2022-06-01 10:40:47 -0400

    avance 4

--- Reinas.java
+++ Reinas.java
@@ -1,9 +1,14 @@
 package Taller;
 
+import java.net.SocketAddress;
 import java.util.Scanner;
 
 public class Reinas {
     public static void main(String args[]) {
+
+        // Para compilar en CMD:
+        // 1°: javac Reinas.java
+        // 2°: java Reinas.java
 
         // Solicita al usuario el tamaño
         Scanner scan = new Scanner(System.in);
@@ -35,7 +40,7 @@ public class Reinas {
         // Recorrido de columnas j
         for (int j = 0; j < num; j++) {
             // Por cada columna recorrida inserta una posicion disponible
-            matriz[j][fila] = "| |";
+            matriz[j][fila] = " ";
             // Si llega a la última columna pero aún no termina a nivel de filas, sigue
             // rellenando
             if (j == num - 1 && fila != num - 1) {
@@ -51,7 +56,7 @@ public class Reinas {
             // Pregunta si la posición actual es válida
             if (valido(tablero, j, fila, num)) {
                 // Si es válida la marca y pasa a la siguiente fila
-                tablero[j][fila] = "|R|";
+                tablero[j][fila] = "R";
                 // Si la fila es menor al valor N, sigue a la siguiente fila
                 if (fila < num - 1) {
                     // Recursión con la siguiente fila
@@ -65,7 +70,7 @@ public class Reinas {
             if (fila < num - 1) {
                 // Si la fila siguiente contiene reinas insertadas, no vacía la posición
                 if (verificador(tablero, fila + 1, num)) {
-                    tablero[j][fila] = "| |";
+                    tablero[j][fila] = " ";
                 }
             }
         }
@@ -76,7 +81,7 @@ public class Reinas {
     public static boolean verificador(String[][] tablero, int fila, int num) {
         // Revisa por cada columna de la fila si contiene reinas, si hay, return false
         for (int j = 0; j < num; j++) {
-            if (tablero[j][fila] == "|R|") {
+            if (tablero[j][fila] == "R") {
                 return false;
             }
         }
@@ -85,42 +90,52 @@ public class Reinas {
     }
 
     public static void impresionMatriz(String[][] tablero, int fila, int num) {
+        System.out.print("|");
         // Recorrido por columnas
         for (int j = 0; j < num; j++) {
             // Si es la ultima columna, salta print hacia otra fila
             if (j == num - 1) {
-                System.out.println(tablero[j][fila] + " ");
+                System.out.println(tablero[j][fila] + "|");
                 // Si fila aún no coincide con tamaño total, pasa a la siguiente
                 if (fila != num - 1) {
                     impresionMatriz(tablero, fila + 1, num);
                 }
                 // Se continúa recorriendo el tablero
             } else {
-                System.out.print(tablero[j][fila] + " , ");
+                System.out.print(tablero[j][fila] + "|");
             }
         }
     }
 
     public static boolean valido(String[][] tablero, int colum, int fila, int num) {
+        // Recorre todas las filas anteriores a la actual
+        for (int y = 0; y < fila; y++) {
+            // Verifica en cada fila de la columna si existe alguna reina
+            if (tablero[colum][y] == "R") {
+                return false;
+            }
+        }
+        // Recorre todas las columnas anteriores a la actual
+        for (int y = 0; y < colum; y++) {
+            if (tablero[y][fila] == "R") {
+                return false;
+            }
+        }
+
         // Se recorre a partir de una columna de la matriz con su valor N (num)
         // Comprobando que sea o no una posicion válida
         for (int x = 0; x < num; x++) {
 
-            // Verifica en cada fila de la columna si existe alguna reina
-            if (tablero[colum][x] == "|R|") {
-                return false;
-            }
-
-            //
+            // Recorre la diagonal derecha superior
             if (colum + x < num && fila - x >= 0) {
-                if (tablero[colum + x][fila - x] == "|R|") {
+                if (tablero[colum + x][fila - x] == "R") {
                     return false;
                 }
             }
 
-            //
+            // Recorre la diagonal izquierda superior
             if (colum - x >= 0 && fila - x >= 0) {
-                if (tablero[colum - x][fila - x] == "|R|") {
+                if (tablero[colum - x][fila - x] == "R") {
                     return false;
                 }
             }
